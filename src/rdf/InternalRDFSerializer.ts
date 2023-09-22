81d860d349e016a4b9d3a60f6ede693f23c15865
Maxim Van de Wynckel <maxim.van.de.wynckel@vub.be>
2023-09-22 18:41:06 +0200

    fix: typedoc and unit fix

--- src/rdf/InternalRDFSerializer.ts
+++ src/rdf/InternalRDFSerializer.ts
@@ -191,10 +191,6 @@ export class InternalRDFSerializer extends Serializer {
                 });
             }
         });
-
-        if (Object.values(thing.predicates).length === 0) {
-            return undefined;
-        }
         return thing;
     }
 
