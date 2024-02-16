6fb7cc44eaf290d40ebdd5b84ba7896e24d7e5ca
Maxim Van de Wynckel <maxim.van.de.wynckel@vub.be>
2024-02-16 13:49:28 +0100

    fix: web

--- src/models/utils.ts
+++ src/models/utils.ts
@@ -6,6 +6,9 @@
 export function applyMixins(derivedCtor: any, constructors: any[]) {
     constructors.forEach((baseCtor) => {
         Object.getOwnPropertyNames(baseCtor.prototype).forEach((name) => {
+            if (derivedCtor.prototype[name]) {
+                return;
+            }
             Object.defineProperty(
                 derivedCtor.prototype,
                 name,
